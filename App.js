305918050cf2c282ee37ddad929aad4fe46bed0e
Lucio <lucioflaviodarocha@gmail.com>
2020-10-01 20:14:36 -0300

    trabalho telas

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text, TouchableHighlight,Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+function HomeScreen1({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center',}}>
+      <Image
+       style = {{
+         width: 350,
+         height: 400,
+       }}
+       source={require('./src/images/ubereats.png')}
+
+      />
+      <TouchableHighlight
+      
+        onPress={() =>navigation.navigate('Home')}
+        style={{
+          backgroundColor: '#3555b6',
+          marginHorizontal: 30,
+          marginTop: 30,
+          padding: 10,
+          borderRadius: 10,
+        }}>
+          <Text style={{fontSize: 18, alignSelf: 'center', margin: 5}}>
+          Login
+          </Text> 
+      </TouchableHighlight>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen2({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center',}}>
+       <Image
+         style = {{
+           width: 350,
+           height: 400,
+         }}
+       
+           source={require('./src/images/ubereats2.png')}
+         />
+         <TouchableHighlight
+           
+          onPress={() =>navigation.navigate('Home')}
+          style={{
+          backgroundColor: '#3555b6',
+          marginHorizontal: 30,
+          marginTop: 30,
+          padding: 10,
+          borderRadius: 10,
+        }}>
+        <Text style={{fontSize: 18, alignSelf: 'center', margin: 5}}>
+          Comprar
+        </Text> 
+      </TouchableHighlight>
+
+    </View>
+  );
+}
+    
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={HomeScreen1} />
+      <Stack.Screen name="Home" component={HomeScreen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
